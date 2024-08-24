bb0456db78985f63f198d1f04b69223e3957310f
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-23 18:16:54 -0700

    LightingState depth_cue interpolation.

--- src/bundles/graphics/src/gsession.py
+++ src/bundles/graphics/src/gsession.py
@@ -323,6 +323,9 @@ class LightingState:
                 elif light_attr == "ambient_light_intensity":
                     lerp_val = num_frac_lerp(scene1[light_attr], scene2[light_attr], frac)
                     setattr(lighting, light_attr, lerp_val)
+                elif light_attr == "depth_cue":
+                    lerp_val = threshold_frac_lerp(scene1[light_attr], scene2[light_attr], frac)
+                    setattr(lighting, light_attr, lerp_val)
 
 
 
