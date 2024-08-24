3334c5ee099cea3062abfe8616c6e6aac039f9b5
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-23 18:06:53 -0700

    LightingState fill_light_color interpolation.

--- src/bundles/graphics/src/gsession.py
+++ src/bundles/graphics/src/gsession.py
@@ -311,6 +311,9 @@ class LightingState:
                 elif light_attr == "fill_light_direction":
                     lerp_val = list_frac_lerp(scene1[light_attr], scene2[light_attr], frac)
                     setattr(lighting, light_attr, lerp_val)
+                elif light_attr == "fill_light_color":
+                    lerp_val = list_frac_lerp(scene1[light_attr], scene2[light_attr], frac)
+                    setattr(lighting, light_attr, lerp_val)
 
 
 
