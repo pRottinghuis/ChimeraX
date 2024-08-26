fa53d8d6ba70419eecd60df307dd60a7b45349a4
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-26 15:01:04 -0700

    Set up AnimationsTool.

---
+++ src/bundles/animations/src/__init__.py
@@ -0,0 +1,25 @@
+from chimerax.core.toolshed import BundleAPI
+from tool import AnimationsTool
+
+
+class _MyAPI(BundleAPI):
+    api_version = 0
+
+    # Override method
+    @staticmethod
+    def start_tool(session, bi, ti):
+        if ti.name == "Animations":
+            from . import tool
+            return AnimationsTool(session, ti.name)
+        raise ValueError("trying to start unknown tool: %s" % ti.name)
+
+    @staticmethod
+    def get_class(class_name):
+        # class_name will be a string
+        if class_name == "AnimationsTool":
+            return AnimationsTool
+        raise ValueError("Unknown class name '%s'" % class_name)
+
+
+# Create the ``bundle_api`` object that ChimeraX expects.
+bundle_api = _MyAPI()
---
+++ src/bundles/animations/src/tool.py
@@ -0,0 +1,35 @@
+
+from chimerax.core.tools import ToolInstance
+
+
+class TutorialTool(ToolInstance):
+
+    SESSION_ENDURING = False    # Does this instance persist when session closes
+    SESSION_SAVE = True         # We do save/restore in sessions
+
+    def __init__(self, session, tool_name):
+        # 'session'   - chimerax.core.session.Session instance
+        # 'tool_name' - string
+
+        super().__init__(session, tool_name)
+
+        # Set name displayed on title bar (defaults to tool_name)
+        # Must be after the superclass init, which would override it.
+        self.display_name = "Animations"
+
+        from chimerax.ui import MainToolWindow
+        self.tool_window = MainToolWindow(self)
+
+    def take_snapshot(self, session, flags):
+        return {
+            'version': 1
+        }
+
+    @classmethod
+    def restore_snapshot(class_obj, session, data):
+        # Instead of using a fixed string when calling the constructor below, we could
+        # have saved the tool name during take_snapshot() (from self.tool_name, inherited
+        # from ToolInstance) and used that saved tool name.  There are pros and cons to
+        # both approaches.
+        inst = class_obj(session, "Animations")
+        return inst
