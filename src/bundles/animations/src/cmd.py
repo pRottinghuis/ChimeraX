b4c7ae4477d8ee2bd0cfdb58e1a386ea19deb6e2
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-27 12:24:06 -0700

    Add edit option to the keyframe command. Edit logging messages and refactored param names.

--- src/bundles/animations/src/cmd.py
+++ src/bundles/animations/src/cmd.py
@@ -12,22 +12,34 @@ def register_command(command_name, logger):
     register(command_name, desc, func)
 
 
-def keyframe(session, action: str, scene_name: str, time: int | float):
+def keyframe(session, action: str, keyframe_name: str, time: int | float):
     """
     Chimnerax Command for edit actions on a keyframe in the animation StateManager. add/edit/delete keyframes.
     Add keyframe will use the scenes scene command to create a new scene to add to the state manager.
     :param session: The current session.
     :param action: The action to take on the keyframe. Options are add, edit, delete.
-    :param scene_name: Name of the keyframe for the action to be applied to. This will also be used for the scene name.
+    :param keyframe_name: Name of the keyframe for the action to be applied to. This will also be used for the scene name.
     :param time: The time in seconds for the keyframe.
     """
+
+    animation_mgr = session.get_state_manager("animations")
 
     if action == "add":
         if not isinstance(time, (int, float)):
             print("Time must be an integer or float")
-
-        run(session, f"scenes scene {scene_name}")
-        session.get_state_manager("animations").add_keyframe(scene_name, time)
+            return
+        run(session, f"scenes scene {keyframe_name}")
+        animation_mgr.add_keyframe(keyframe_name, time)
+    elif action == "edit":
+        if not animation_mgr.keyframe_exists(keyframe_name):
+            print(f"Keyframe {keyframe_name} does not exist")
+            return
+        if not isinstance(time, (int, float)):
+            print("Time must be an integer or float")
+            return
+        animation_mgr.edit_keyframe_time(keyframe_name, time)
+    else:
+        print(f"Action {action} not recognized. Options are add, edit, delete.")
 
 
 keyframe_desc = CmdDesc(
