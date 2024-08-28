5ec473c5829c14bbacb2cc8f075a37ef82019ede
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-27 23:42:33 -0700

    Add a command to preview a animation at a timestamp.

--- src/bundles/animations/src/cmd.py
+++ src/bundles/animations/src/cmd.py
@@ -96,3 +96,30 @@ def play(session):
 play_desc = CmdDesc(
     synopsis="Play the animation."
 )
+
+
+def preview(session, time: int | float):
+    """
+    Preview the animation at a specific time.
+    :param session: The current session.
+    :param time: The time in seconds to preview the animation.
+    """
+    animation_mgr = session.get_state_manager("animations")
+    if not isinstance(time, (int, float)):
+        print("Time must be an integer or float")
+        return
+    if animation_mgr.get_num_keyframes() < 1:
+        print("Need at least 1 keyframes to preview the animation.")
+        return
+    if not animation_mgr.time_in_range(time):
+        print(f"Time must be between 0 and {animation_mgr.get_time_length()}")
+        return
+    animation_mgr.preview(time)
+
+
+preview_desc = CmdDesc(
+    required=[
+        ("time", FloatArg)
+    ],
+    synopsis="Preview the animation at a specific time."
+)
