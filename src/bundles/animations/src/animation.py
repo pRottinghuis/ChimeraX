b39b55ac43ded51f85d25c191fdcd28d30caf36f
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-27 22:03:24 -0700

    Make sure that when generating interpolation steps there is at least one keyframe in the animation.

--- src/bundles/animations/src/animation.py
+++ src/bundles/animations/src/animation.py
@@ -65,6 +65,9 @@ class Animation(StateManager):
 
     def _gen_lerp_steps(self):
         # TODO make sure that there is at least 1 keyframe in the animation
+        if len(self.keyframes) < 1:
+            self.session.logger.warning(f"Can't generate lerp steps because there are no keyframes.")
+            return
 
         # tuple val to store previously iterated keyframe (keyframe name, time).
         prev_kf_name = None
