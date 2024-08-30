0490fae06da411e6f06afdd33836897677ecc0f8
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-08-30 15:32:02 -0700

    [feature] Experimental jpeg QGraphicsPixmapItem
    Proof of concept for displaying jpeg_data from scene keyframes in a qt pixmap.
    Experimenting with Timeline widget and keyframe QGraphicsPixmapItem for developing gui.

--- src/bundles/animations/src/tool.py
+++ src/bundles/animations/src/tool.py
@@ -1,10 +1,13 @@
 from chimerax.core.tools import ToolInstance
+from Qt.QtWidgets import QGridLayout, QLabel, QGraphicsPixmapItem, QGraphicsItem, QGraphicsView, QGraphicsScene, \
+    QVBoxLayout, QWidget
+from Qt.QtCore import QByteArray, Qt, QPointF
+from Qt.QtGui import QPixmap
 
 
 class AnimationsTool(ToolInstance):
-
-    SESSION_ENDURING = False    # Does this instance persist when session closes
-    SESSION_SAVE = True         # We do save/restore in sessions
+    SESSION_ENDURING = False  # Does this instance persist when session closes
+    SESSION_SAVE = True  # We do save/restore in sessions
 
     def __init__(self, session, tool_name):
         # 'session'   - chimerax.core.session.Session instance
@@ -19,6 +22,16 @@ class AnimationsTool(ToolInstance):
         from chimerax.ui import MainToolWindow
         self.tool_window = MainToolWindow(self)
 
+        # test scene thumbnails
+        self.build_ui()
+
+        self.tool_window.manage("side")
+
+    def build_ui(self):
+        vbox = QVBoxLayout()
+        vbox.addWidget(TimelineWidget())
+        self.tool_window.ui_area.setLayout(vbox)
+
     def take_snapshot(self, session, flags):
         return {
             'version': 1
@@ -32,3 +45,38 @@ class AnimationsTool(ToolInstance):
         # both approaches.
         inst = class_obj(session, "Animations")
         return inst
+
+
+class KeyframeItem(QGraphicsPixmapItem):
+    def __init__(self, pixmap, position):
+        super().__init__(pixmap)
+        self.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemSendsGeometryChanges)
+        self.setPos(position)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionChange:
+            # set y to keep the keyframe on the timeline
+            return QPointF(value.x(), 0)
+        return super().itemChange(change, value)
+
+
+class TimelineWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.layout = QVBoxLayout(self)
+        self.timeline_view = QGraphicsView(self)
+        self.timeline_scene = QGraphicsScene(self)
+        self.timeline_view.setScene(self.timeline_scene)
+        self.layout.addWidget(self.timeline_view)
+
+        # Example keyframes
+        for i in range(5):
+            pixmap = QPixmap(50, 50)
+            pixmap.fill(Qt.red)
+            keyframe = KeyframeItem(pixmap, QPointF(i * 60, 0))
+            self.timeline_scene.addItem(keyframe)
+
+        self.setLayout(self.layout)
