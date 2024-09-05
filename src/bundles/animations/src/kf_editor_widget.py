f3f51b1349b1cd509599108751247a00f7af22a8
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-09-04 23:26:01 -0700

    [refactor] KeyframeEditorWidget class layout
    The KeyframeEditorWidget is in it's own file for furthur development. In addition,
    the tickmarks are moved into their own GraphicsItemGroup and there is a custom
    implementation for the graphics scene.

---
+++ src/bundles/animations/src/kf_editor_widget.py
@@ -0,0 +1,94 @@
+from Qt.QtWidgets import QGridLayout, QLabel, QGraphicsPixmapItem, QGraphicsItem, QGraphicsView, QGraphicsScene, \
+    QVBoxLayout, QWidget, QGraphicsTextItem, QGraphicsLineItem, QGraphicsItemGroup
+from Qt.QtCore import QByteArray, Qt, QPointF, QLineF
+from Qt.QtGui import QPixmap, QPen
+
+
+class KeyframeEditorWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout(self)
+        self.kfe_view = QGraphicsView(self)
+        self.kfe_scene = KeyframeEditorScene()
+        self.kfe_view.setScene(self.kfe_scene)
+        self.layout.addWidget(self.kfe_view)
+
+
+class KeyframeEditorScene(QGraphicsScene):
+    def __init__(self):
+        super().__init__()
+        for i in range(5):
+            pixmap = QPixmap(50, 50)
+            pixmap.fill(Qt.red)
+            keyframe = KeyframeItem(pixmap, QPointF(i * 60, 0))
+            self.addItem(keyframe)
+
+        self.addItem(Timeline())
+
+
+class KeyframeItem(QGraphicsPixmapItem):
+    def __init__(self, pixmap, position):
+        super().__init__(pixmap)
+        self.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemSendsGeometryChanges)
+        self.setPos(position)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionChange:
+            # Called before the position has changed.
+            # set y to keep the keyframe on the timeline
+            return QPointF(value.x(), 0)
+        elif change == QGraphicsItem.ItemPositionHasChanged:
+            pass
+            # Called after the position has changed.
+        return super().itemChange(change, value)
+
+
+class Timeline(QGraphicsItemGroup):
+    def __init__(self, length=600, interval=10, major_interval=100, tick_length=10, major_tick_length=20):
+        super().__init__()
+        self.length = length
+        self.interval = interval
+        self.major_interval = major_interval
+        self.tick_length = tick_length
+        self.major_tick_length = major_tick_length
+        self.update_tick_marks()
+
+    def update_tick_marks(self):
+        y_position = 60  # Top positions of the tick marks and labels on the y-axis
+
+        # Clear existing tick marks and labels
+        for item in self.childItems():
+            self.removeFromGroup(item)
+            self.scene().removeItem(item)
+
+        for i in range(0, self.length, self.interval):
+            position = QPointF(i, y_position)
+            if i % self.major_interval == 0:
+                tick_mark = QGraphicsLineItem(
+                    QLineF(position, QPointF(position.x(), position.y() + self.major_tick_length)))
+                tick_mark.setPen(QPen(Qt.gray, 1))
+                self.addToGroup(tick_mark)
+
+                time_label = QGraphicsTextItem(f"{i // self.major_interval}")
+                text_rect = time_label.boundingRect()
+                time_label.setPos(position.x() - text_rect.width() / 2, y_position + self.major_tick_length)
+                self.addToGroup(time_label)
+            else:
+                tick_mark = QGraphicsLineItem(QLineF(position, QPointF(position.x(), position.y() + self.tick_length)))
+                tick_mark.setPen(QPen(Qt.gray, 1))
+                self.addToGroup(tick_mark)
+
+    def set_length(self, length):
+        self.length = length
+        self.update_tick_marks()
+
+
+class TickMarkItem(QGraphicsLineItem):
+    def __init__(self, position, length):
+        super().__init__(QLineF(position, QPointF(position.x(), position.y() + length)))
+        self.setPen(QPen(Qt.gray, 1))
+
+
+class TickIntervalLabel(QGraphicsTextItem):
+    def __init__(self, text):
+        super().__init__(text)
--- src/bundles/animations/src/tool.py
+++ src/bundles/animations/src/tool.py
@@ -29,6 +29,7 @@ class AnimationsTool(ToolInstance):
 
     def build_ui(self):
         vbox = QVBoxLayout()
+        from .kf_editor_widget import KeyframeEditorWidget
         vbox.addWidget(KeyframeEditorWidget())
         self.tool_window.ui_area.setLayout(vbox)
 
@@ -45,81 +46,3 @@ class AnimationsTool(ToolInstance):
         # both approaches.
         inst = class_obj(session, "Animations")
         return inst
-
-
-class KeyframeItem(QGraphicsPixmapItem):
-    def __init__(self, pixmap, position):
-        super().__init__(pixmap)
-        self.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemSendsGeometryChanges)
-        self.setPos(position)
-
-    def itemChange(self, change, value):
-        if change == QGraphicsItem.ItemPositionChange:
-            # Called before the position has changed.
-            # set y to keep the keyframe on the timeline
-            return QPointF(value.x(), 0)
-        elif change == QGraphicsItem.ItemPositionHasChanged:
-            pass
-            # Called after the position has changed.
-        return super().itemChange(change, value)
-
-
-class TickMarkItem(QGraphicsLineItem):
-    def __init__(self, position, length):
-        super().__init__(QLineF(position, QPointF(position.x(), position.y() + length)))
-        self.setPen(QPen(Qt.gray, 1))
-
-
-class TickIntervalLabel(QGraphicsTextItem):
-    def __init__(self, text):
-        super().__init__(text)
-
-
-class KeyframeEditorWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.length = 600
-        self.init_ui()
-
-    def init_ui(self):
-        self.layout = QVBoxLayout(self)
-        self.timeline_view = QGraphicsView(self)
-        self.timeline_scene = QGraphicsScene(self)
-        self.timeline_view.setScene(self.timeline_scene)
-        self.layout.addWidget(self.timeline_view)
-
-        # Example keyframes
-        for i in range(5):
-            pixmap = QPixmap(50, 50)
-            pixmap.fill(Qt.red)
-            keyframe = KeyframeItem(pixmap, QPointF(i * 60, 0))
-            self.timeline_scene.addItem(keyframe)
-
-        self.add_tick_marks()
-
-        self.setLayout(self.layout)
-
-    def add_tick_marks(self):
-        y_position = 60  # Top positions of the tick marks and labels on the y-axis
-        interval = 10  # Interval between tick marks in pixels
-        major_interval = 100  # Interval between major tick marks in pixels
-        tick_length = 10  # Length of minor tick marks
-        major_tick_length = 20  # Length of major tick marks
-
-        # Clear existing tick marks and labels
-        for item in self.timeline_scene.items():
-            if isinstance(item, (TickMarkItem, TickIntervalLabel)):
-                self.timeline_scene.removeItem(item)
-
-        for i in range(0, self.length, interval):
-            position = QPointF(i, y_position)
-            if i % major_interval == 0:
-                tick_mark = TickMarkItem(position, major_tick_length)
-                time_label = TickIntervalLabel(f"{i // major_interval}")
-                # Use the boundingRect dimensions to center the text under a major tick mark
-                text_rect = time_label.boundingRect()
-                time_label.setPos(position.x() - text_rect.width() / 2, y_position + major_tick_length)
-                self.timeline_scene.addItem(time_label)
-            else:
-                tick_mark = TickMarkItem(position, tick_length)
-            self.timeline_scene.addItem(tick_mark)
